7a348f97996bd4bd04657080682fdc5a6888f151
Nicola <nicolamascarenhas1@gmail.com>
2024-08-14 01:22:37 +0530

    Added Spring Application & Entity

---
+++ ContactReconcilator/src/main/java/com/bitespeedapplication/ContactEntity.java
@@ -0,0 +1,101 @@
+package com.bitespeedapplication;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contact")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ContactEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+
+	long phoneNumber;
+
+	String email;
+
+	int linkedId;
+
+	String linkPrecedence;
+
+	LocalDateTime createdAt; // could also store date as epoch & will convert to Timestamp 2023-04-20
+								// 05:30:00.11+00
+
+	LocalDateTime updatedAt;
+
+	LocalDateTime deletedAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getLinkedId() {
+		return linkedId;
+	}
+
+	public void setLinkedId(int linkedId) {
+		this.linkedId = linkedId;
+	}
+
+	public String getLinkPrecedence() {
+		return linkPrecedence;
+	}
+
+	public void setLinkPrecedence(String linkPrecedence) {
+		this.linkPrecedence = linkPrecedence;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDateTime getDeletedAt() {
+		return deletedAt;
+	}
+
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+}
